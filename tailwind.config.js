b8dccb663b2d19b66889b224c1123475d896cebf
STD-mathished <mathis.heder91@gmail.com>
2025-07-02 18:12:39 +0200

    front end 8°%

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        pomodoro: '#E94E77',
+        app: '#b3bcbb',
+        btn: '#b3bcbb',
+        bgBtn: '#b3bcbb'
+      },
+    },
+  },
+  plugins: [],
+};
